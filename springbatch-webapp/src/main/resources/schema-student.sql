3db44b0c092e085369d503e9b6e260bf7a38e252
carlosrgomes <carlosrgomes@gmail.com>
2015-07-16 13:07:12 -0300

    atualizando

--- springbatch-webapp/src/main/resources/schema-student.sql
+++
@@ -1,7 +0,0 @@
-DROP TABLE IF EXISTS EXAM_RESULT;
-create table EXAM_RESULT (
-   id bigint auto_increment,
-   student_name VARCHAR(30) NOT NULL,
-   dob DATE NOT NULL,
-   percentage  double NOT NULL
-);
